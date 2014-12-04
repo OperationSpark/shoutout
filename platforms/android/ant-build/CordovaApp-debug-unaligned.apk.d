1dc3de11a87b1b59ede7a76509c666591cfac574
jfraboni <johnfraboni@hotmail.com>
2014-12-04 00:21:19 -0600

    * add completed shout feature

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes.dex \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/CordovaLib/ant-build/classes.jar \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,14 @@
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes.dex : \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$1.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$2.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$3.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionicframework/shoutout237577/BuildConfig.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionicframework/shoutout237577/CordovaApp.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionicframework/shoutout237577/R$attr.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionicframework/shoutout237577/R$drawable.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionicframework/shoutout237577/R$string.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionicframework/shoutout237577/R$xml.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/com/ionicframework/shoutout237577/R.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
+/Users/jfraboni/work/spark/projects/shoutout/platforms/android/ant-build/dexedLibs/classes-54952f830c8fe2a3b0281d5df10e0205.jar \
